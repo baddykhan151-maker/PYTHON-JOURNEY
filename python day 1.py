22a89c72cbd78b41317f4e69f16dba7f186cd5a2
baddykhan151-maker <baddykhan151@gmail.com>
2026-07-14 13:26:37 -0700

    Day 1: python basics - print statements
    First day of Python - learned print() function, strings, and basic output. No prior coding background.

---
+++ python day 1.py
@@ -0,0 +1,12 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello Bro")
+Hello Bro
+>>> print("Name:Sohail")
+Name:Sohail
+>>> print("Age:18")
+Age:18
+>>> print("Welcome:My Store")
+Welcome:My Store
+>>> print("Owner name:Sohail")
+Owner name:Sohail
